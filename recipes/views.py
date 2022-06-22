9dec4ffd9064c8304d12dd1789ed1680394f91b0
macwdo <danilo.macedofernandes@hotmail.com>
2022-06-22 18:20:41 -0300

    a

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+
+def contato(request):
+    return HttpResponse('123231232-22')
+
+
+def sobre(request):
+    return HttpResponse('Sobre nós')
+
