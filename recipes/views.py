cee63a332f5c95908e12bc8e297a072d210c1e2b
macwdo <danilo.macedofernandes@hotmail.com>
2022-06-23 04:33:40 -0300

    Começando site

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'nome':'Danilo'})
-
-
-def contato(request):
-    return render(request, 'recipes/temp.html',context={'numero':'993406469'})
-
-
-def sobre(request):
-    return HttpResponse('Sobre nós')
-
+    return render(request, 'recipes/home.html', context={'nome': 'Danilo'})
