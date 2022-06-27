89aebbeb8151e9d9a44a8945c790a1960c2eb636
macwdo <danilo.macedofernandes@hotmail.com>
2022-06-27 10:31:25 -0300

    tudo

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'nome': 'Macedo'})
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipes-view.html', context={'nome': 'Macedo'})
