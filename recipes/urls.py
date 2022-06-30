fca1d55eeb881200a6b1d1fa9d006e420f6e34d5
macwdo <danilo.macedofernandes@hotmail.com>
2022-06-30 12:52:58 -0300

    err

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('<int:id>/', views.recipe, name="recipe"),
+    path('category/<int:category_id>/', views.category, name="category"),
     path('recipes/', views.home, name='home')
     ]
