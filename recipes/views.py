20466ce2459f637160b2ae516da69cde75ab987a
macwdo <danilo.macedofernandes@hotmail.com>
2022-06-30 19:38:24 -0300

    ss

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import  Recipe
 # Create your views here.
@@ -12,12 +12,11 @@ def home(request):
 
 def category(request,category_id):
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id,is_published=True).order_by('-id'),category__id=category_id,is_published=True)
-
+    
     return render(request, 'recipes/pages/category.html', context={'recipes': recipes , 'title' : f'{recipes[0].category.name}'})
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(pk=id,is_published=True).order_by('-id').first()
-
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes/pages/recipes-view.html', context={'recipe' : recipe, "is_detail_page" : True})
